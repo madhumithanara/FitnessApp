4055f8997998795f1d070cc34b2c3ac257bd7b41
Madhumitha Nara <madhumithanara.171it125@nitk.edu.in>
2021-02-11 19:44:05 +0530

    Fixed all redirection issues

--- fitnessWebsite/accounts/urls.py
+++ fitnessWebsite/accounts/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = "accounts"
+app_name = 'accounts'
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(template_name="signup.html"), name='signup'),
+    path('signup/', views.SignUp.as_view(template_name='registration/signup.html'), name='signup'),
     path('profile/goals/', views.goals, name='profile/goals'),
     path('profile/energy_analysis',views.energy_analysis,name='profile/energy_analysis'),
 ]
